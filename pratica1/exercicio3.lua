90db00de9273fc56a0955ba3074492254fc245ee
hugombarreto <hugombarreto@gmail.com>
2013-08-30 23:11:04 -0300

    Create exercicio3.lua

---
+++ pratica1/exercicio3.lua
@@ -0,0 +1,40 @@
+-- exercicio3.lua
+--
+-- Linguagens de Prgramacao - UFRJ
+-- Hugo F S Sadok Menna Barreto
+
+NUMERO_DADOS_ALEATORIOS = 100
+
+function quickSort(array, ini, fim)
+	local i, j = ini, fim
+	local pivot = array[math.floor((fim+ini)/2)]--> nao usa o meio
+	while i <= j do
+		while array[i] < pivot do
+			i = i + 1
+		end
+		while array[j] > pivot do
+			j = j -1
+		end
+		if i <= j then
+			array[i], array[j] = array[j], array[i]
+			i = i + 1
+			j = j - 1
+		end
+	end
+	if j > ini then quickSort(array, ini, j) end
+	if i < fim then quickSort(array, i, fim) end--> diferente
+end
+
+local listaDeNumerosAleatorios = {}
+
+math.randomseed(os.time())
+for i=1, NUMERO_DADOS_ALEATORIOS do
+	listaDeNumerosAleatorios[i] = math.random(2000)
+end
+
+quickSort(listaDeNumerosAleatorios, 1, NUMERO_DADOS_ALEATORIOS)
+
+for k, v in pairs(listaDeNumerosAleatorios) do
+	io.write(v, "  ")
+end
+print()
