081243134930660890c8c2c7f13dee25414a07a1
Hugo Menna Barreto <hugombarreto@gmail.com>
2013-09-10 17:03:04 -0300

    Create 2combination.h

---
+++ lista4/2combination.h
@@ -0,0 +1,16 @@
+/*
+ * Lista 4
+ * 2 - Combination
+ * 2combination.h
+ * Hugo F S Sadok Menna Barreto
+ */
+
+#ifndef COMBINATION_H
+#define COMBINATION_H
+
+/**  ___ n!___
+ *   (n-s)! s!
+ */
+double combination(unsigned n, unsigned s);
+
+#endif
