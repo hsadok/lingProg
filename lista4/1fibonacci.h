e28a52fb8ff3d94427e3f705520ceeae71d69fe1
Hugo Menna Barreto <hugombarreto@gmail.com>
2013-09-10 17:00:29 -0300

    Create 1fibonacci.h

---
+++ lista4/1fibonacci.h
@@ -0,0 +1,13 @@
+/*
+ * Lista 4
+ * 1 - Fibonacci
+ * 1fibonacci.h
+ * Hugo F S Sadok Menna Barreto
+ */
+
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+
+unsigned fibonacci(unsigned numElementos);
+
+#endif
