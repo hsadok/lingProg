607ad0460e8caedc5dcc378ac7abc38edc50bbd5
Hugo Menna Barreto <hugombarreto@gmail.com>
2013-09-10 17:05:11 -0300

    Create 3lista.h

---
+++ lista4/3lista.h
@@ -0,0 +1,31 @@
+/*
+ * Lista 4
+ * 3 - Lista
+ * 3lista.h
+ * Hugo F S Sadok Menna Barreto
+ */
+
+#ifndef LISTA_H
+#define LISTA_H
+
+using namespace std;
+
+typedef struct nodeStruct{
+	string name;
+	string phone;
+	struct nodeStruct *next;
+
+} nodeType;
+
+class Lista{
+	public:
+		Lista();
+		bool addElement(string nome, string telefone);
+		void cleanList();
+		void printList();
+	private:
+		nodeType *listaFinal;
+		nodeType *listaInicio; 
+};
+
+#endif
