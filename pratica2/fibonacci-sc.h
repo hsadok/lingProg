34ee325c405131c91a698f09127fc39c4f9e2683
Hugo Menna Barreto <hugombarreto@gmail.com>
2013-09-15 11:21:45 -0300

    Create fibonacci-sc.h

---
+++ pratica2/fibonacci-sc.h
@@ -0,0 +1,13 @@
+/*
+ * Pratica 2
+ * 1 - Fibonacci
+ * fibonacci-sc.h
+ * Hugo F S Sadok Menna Barreto
+ */
+
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+
+unsigned fibonacci(unsigned numElementos);
+
+#endif
